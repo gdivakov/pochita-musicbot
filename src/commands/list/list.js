9d7c06d77f04e11f164c961a6931cd351fe7ce3b
Dnll <danielkostenko0305@gmail.com>
2023-06-16 14:03:52 +0200

    fix list where user isn`t in voice channel

--- src/commands/list/list.js
+++ src/commands/list/list.js
@@ -10,12 +10,16 @@ module.exports = {
         try {
             const queue = client.player.queues.get(interaction.guildId);
 
+            if(!queue) {
+                await interaction.reply('There is no queue');
+                return;
+            }
+
             const prevTracks = queue.history.tracks.data.slice(0, PREV_TRACKS_TO_SHOW_NUM).reverse().map(prepareSongTitle).join('\n\t');
             const currentTrack = queue.currentTrack;
             const nextTracks = queue.tracks.data.slice(0, NEXT_TRACKS_TO_SHOW_NUM).map(prepareSongTitle).join('\n\t');
 
             const displayedQueue = '\t' + prevTracks + '\n' + '> ' + prepareSongTitle(currentTrack) + '\n\t' + nextTracks;
-
             await interaction.reply(`Queue list: \n${displayedQueue}`)
         } catch(e) {
             return interaction.reply(`list error ${e}`)
