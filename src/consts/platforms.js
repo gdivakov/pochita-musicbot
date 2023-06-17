903112e7a93ed62755627607cb8870a9a2216433
gdivakov <divakov.gleb@gmail.com>
2023-06-17 04:09:54 +0300

    \supported-platforms added

---
+++ src/consts/platforms.js
@@ -0,0 +1,10 @@
+const SUPPORTED_PLATFORMS = {
+  REVERBNATION: 'Reverbnation',
+  SOUNDCLOUD: 'SoundCloud',
+  VIMEO: 'Vimeo',
+  YOUTUBE: 'YouTube',
+  SPOTIFY: 'Spotify',
+  APPLE_MUSIC: 'Apple Music'
+}
+
+module.exports = SUPPORTED_PLATFORMS;
