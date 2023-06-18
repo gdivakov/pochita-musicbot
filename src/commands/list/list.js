3713b2918f8d5b4fb4dd4565651a20c43ab95457
gdivakov <divakov.gleb@gmail.com>
2023-06-18 10:01:45 +0300

    dev: /list error fixed: currentTrack is null

--- src/commands/list/list.js
+++ src/commands/list/list.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { prepareSongTitle } = require('@utils/formatString');
-const { PREV_TRACKS_TO_SHOW_NUM, NEXT_TRACKS_TO_SHOW_NUM } = require('@consts');
+const { MIN_PREV_TRACKS_TO_SHOW, MAX_NEXT_TRACKS_TO_SHOW } = require('@consts');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,11 +13,14 @@ module.exports = {
 			return await interaction.reply('There is no queue');
 		}
 
-		const prevTracks = queue.history.tracks.data.slice(0, PREV_TRACKS_TO_SHOW_NUM).reverse().map(prepareSongTitle).join('\n\t');
-		const currentTrack = queue.currentTrack;
-		const nextTracks = queue.tracks.data.slice(0, NEXT_TRACKS_TO_SHOW_NUM).map(prepareSongTitle).join('\n\t');
+		const currentTrack = queue.currentTrack || queue.history.tracks.data[0];
 
-		const displayedQueue = '\t' + prevTracks + '\n' + '> ' + prepareSongTitle(null) + '\n\t' + nextTracks;
+		const prevTracksToShowNum = queue.currentTrack ? MIN_PREV_TRACKS_TO_SHOW : MIN_PREV_TRACKS_TO_SHOW + 1;
+
+		const prevTracks = queue.history.tracks.data.slice(queue.currentTrack ? 0 : 1, prevTracksToShowNum).reverse().map(prepareSongTitle).join('\n\t');
+		const nextTracks = queue.tracks.data.slice(0, MAX_NEXT_TRACKS_TO_SHOW).map(prepareSongTitle).join('\n\t');
+
+		let displayedQueue = '\t' + prevTracks + '\n> ' + prepareSongTitle(currentTrack) + '\n\t' + nextTracks;
 
 		await interaction.reply(`Queue list: \n${displayedQueue}`);
 	}
