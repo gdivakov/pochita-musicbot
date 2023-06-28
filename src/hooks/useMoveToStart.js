18cc4b862d0f38b679ea7fb4e7d0b5b6f26d0e57
gdivakov <divakov.gleb@gmail.com>
2023-06-28 18:36:00 +0300

    fix-cant-use-play-after-play: fix-hook-useMove

--- src/hooks/useMoveToStart.js
+++ src/hooks/useMoveToStart.js
@@ -3,12 +3,11 @@ const { useQueue } = require('discord-player');
 // Move tracks to start of the queue
 const useMoveToStart = ({ from, num, guildId, withSkip = true }) => {
 	const queue = useQueue(guildId);
-
 	if (!queue) {
 		return false;
 	}
 
-	const isAlreadyInQueue = queue.tracks.data.length <= num;
+	const isAlreadyInQueue = queue.tracks.data.length < num;
 
 	if (isAlreadyInQueue)
 	{
@@ -19,7 +18,6 @@ const useMoveToStart = ({ from, num, guildId, withSkip = true }) => {
 	for (let fromIdx = from, startPos = 0; num > 0; ++fromIdx, ++startPos, --num) {
 		queue.node.move(fromIdx, startPos);
 	}
-
 	if (withSkip)
 	{
 		// Play next song from the queue
